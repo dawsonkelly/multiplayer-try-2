a4b4482b35cd353df2c665b3ce218d6dce593f16
Dawson Kelly <601dtkelly@gmail.com>
2025-09-25 18:02:35 -0400

    Finish

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,13 +3,26 @@ extends Node
 func _ready():
 	if OS.has_feature("dedicated_server"):
 		print("Starting dedicated server")
-		MultiplayerManager.become_host()
-		
+		%NetworkManager.become_host()
 
 func become_host():
 	%MultiplayerHud.hide()
-	MultiplayerManager.become_host()
+	%SteamHud.hide()
+	%NetworkManager.become_host()
 
-func join_as_player_2():
+func join_as_client():
 	%MultiplayerHud.hide()
-	MultiplayerManager.join_as_player_2()
+	%SteamHud.hide()
+	%NetworkManager.join_as_client()
+
+func use_steam():
+	%MultiplayerHud.hide()
+	%SteamHud.show()
+	print("Using Steam")
+	SteamManager.initialize_steam()
+	%NetworkManager.active_network_type = %NetworkManager.MULTIPLAYER_NETWORK_TYPE.STEAM
+
+func list_steam_lobbies():
+	print("List steam lobbies")
+	%NetworkManager.list_steam_lobbies()
+	
